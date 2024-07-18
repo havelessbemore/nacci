9d3f7785394d32be804953b6582f4319933a0457
havelessbemore <dev.michael.rojas@gmail.com>
2024-07-18 15:16:32 -0400

    Update eslint from v8 to v9

--- eslint.config.js
+++ eslint.config.js
@@ -1,14 +1,15 @@
+import globals from "globals";
 import eslint from "@eslint/js";
 import prettierConfig from "eslint-config-prettier";
 import tseslint from "typescript-eslint";
 
-/** @type {import('@typescript-eslint/utils').TSESLint.FlatConfig.ConfigArray} */
-export default tseslint.config(
-  {
-    ignores: ["dist", "docs", "examples"],
-  },
+/** @type {import('@typescript-eslint/utils').TSESLint.FlatConfig.ConfigFile} */
+export default [
+  {ignores: ["coverage", "dist"]},
+  {files: ["**/*.{js,mjs,cjs,ts}"]},
+  {languageOptions: { globals: {...globals.browser, ...globals.node} }},
   eslint.configs.recommended,
   ...tseslint.configs.recommended,
   ...tseslint.configs.stylistic,
   prettierConfig,
-);
+];
